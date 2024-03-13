eb369c1017e534cb94116cd53676f2c33388267c
geb <853934146@qq.com>
2024-03-13 10:59:18 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="langchain_nexus",
+    version="0.0.0",
+    author="geb",
+    author_email="853934146@qq.com",
+    description="Langchain-Nexus is a Python library enabling easy integration with diverse language models like ChatGPT and GLM through a unified interface.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Security-Product/langchain-nexus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['langchain', 'openai', 'zhipuai'],
+    python_requires='>=3.10',
+)
